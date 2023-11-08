494786e96b2629974b38cf518499382093909464
openworld42 <github@silberbauer.xyz>
2023-11-09 00:36:44 +0100

    overworked javadoc

--- javadoc/package-search-index.js
+++ javadoc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"template"},{"l":"template.ui"},{"l":"template.xml"}]
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"template"},{"l":"template.ui"},{"l":"template.xml"}];updateSearchResults();
